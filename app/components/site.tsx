7035446bfcb50fd625fa8d8c78c51ad6ff0c470e
Ravi Banagere <79131947+banagere@users.noreply.github.com>
2025-01-14 12:47:24 +0530

    bump packages

--- app/components/site.tsx
+++ app/components/site.tsx
@@ -2,7 +2,8 @@ const site = {
   name: "Mankuthimmana Kagga",
   website: "https://mankuthimma.com",
   domain: "mankuthimma.com",
-  description: "Musician, Writer and Developer!",
+  description:
+    "An accessible repository for the people who love Mankuthimmana Kagga, written by DV Gundappa.",
 };
 
 export default site;
