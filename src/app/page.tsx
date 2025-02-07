f1c939c9b02eefaba8fec1ab9fd015a0e45d7b23
Ravi Banagere <79131947+banagere@users.noreply.github.com>
2025-02-08 00:30:20 +0530

    slight redesign of homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,41 +22,7 @@ function PostsList({ posts }) {
   );
 }
 
-function InfoSection() {
-  return (
-    <div className="max-w-5xl px-5 py-10 mx-auto text-center">
-      {[
-        "Mankuthimmana Kagga, written by Devanahalli Venkataramanaiah Gundappa (DVG) was first published in 1943. It stands as a prominent piece of Kannada literature. This collection comprises 945 poems, each spanning four lines, with several written in old Kannada (ಹಳೆಗನ್ನಡ).",
-        "This book explores deeper questions of life, reflects on the essence of Ultimate Truth (reality), and suggests leading a balanced life amidst the complexities and constant changes of our world. It encourages adopting a moderate approach, with one hand reaching for the Ultimate Truth and the other engaged with the tangible world.",
-        "Mankuthimmana Kagga fascinated me the first time I read it at school. I wished to revisit it and share some snippets, but I couldn't find it online. This led me to create this website. This treasure should be accessible to all and shouldn't be hidden.",
-        "The content for this site was sourced from books and websites, and all credit goes to the original authors and publishers.",
-        "I earn no money from this website and did this out of gratitude for DVG's craft.",
-        "I hope these Kagga's change your outlook towards life, as it did to mine!",
-        "Please buy the book to support the publishers, printers, editors and authors!",
-        <>
-          Also, this website&apos;s code is publicly accessible. If you&apos;d like to
-          check it, visit{" "}
-          <Link
-            href="https://github.com/banagere/mankuthimma.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Github (opens in a new tab)"
-            className="underline duration-500 hover:opacity-70 underline-offset-4"
-          >
-            Github
-          </Link>
-          .
-        </>,
-      ].map((text, index) => (
-        <p key={index} className={paragraph}>
-          {text}
-        </p>
-      ))}
-    </div>
-  );
-}
-
-export default function Index() {
+export default function Home() {
   const sortedPosts = useMemo(
     () => allPosts?.sort((a, b) => a.weight - b.weight) ?? [],
     []
@@ -68,12 +34,42 @@ export default function Index() {
 
   return (
     <div className="snap-y snap-mandatory">
-      <div className="min-h-screen px-5 pb-5 mx-auto max-w-7xl snap-center snap-always">
-        <PostsList posts={sortedPosts} />
+      <div className="min-h-screen mx-auto max-w-7xl snap-center snap-always">
+        <div className="px-5">
+          <PostsList posts={sortedPosts} />
+        </div>
       </div>
 
       <div className="min-h-screen text-white bg-red-700 snap-center snap-always">
-        <InfoSection />
+        <div className="flex flex-col items-center justify-center max-w-5xl p-5 mx-auto text-center">
+          {[
+            "Mankuthimmana Kagga, written by Devanahalli Venkataramanaiah Gundappa (DVG) was first published in 1943. It stands as a prominent piece of Kannada literature. This collection comprises 945 poems, each spanning four lines, with several written in old Kannada (ಹಳೆಗನ್ನಡ).",
+            "This book explores deeper questions of life, reflects on the essence of Ultimate Truth (reality), and suggests leading a balanced life amidst the complexities and constant changes of our world. It encourages adopting a moderate approach, with one hand reaching for the Ultimate Truth and the other engaged with the tangible world.",
+            "Mankuthimmana Kagga fascinated me the first time I read it at school. I wished to revisit it and share some snippets, but I couldn't find it online. This led me to create this website. This treasure should be accessible to all and shouldn't be hidden.",
+            "The content for this site was sourced from books and websites, and all credit goes to the original authors and publishers.",
+            "I earn no money from this website and did this out of gratitude for DVG's craft.",
+            "I hope these Kagga's change your outlook towards life, as it did to mine!",
+            "Please buy the book to support the publishers, printers, editors and authors!",
+            <>
+              Also, this website&apos;s code is publicly accessible. If
+              you&apos;d like to check it, visit{" "}
+              <Link
+                href="https://github.com/banagere/mankuthimma.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Github (opens in a new tab)"
+                className="underline duration-500 hover:opacity-70 underline-offset-4"
+              >
+                Github
+              </Link>
+              .
+            </>,
+          ].map((text, index) => (
+            <p key={index} className={paragraph}>
+              {text}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
